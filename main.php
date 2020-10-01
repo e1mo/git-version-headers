d23d18c43194f1706c1e969a94fd49c8c418d6a8
Moritz 'e1mo' Fromm <git@e1mo.de>
2020-10-01 19:21:15 +0200

    Include if it's dirty or broken

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <?php
 
-$gitVersion = trim(shell_exec('git -C '. __DIR__ .'/../.. describe --long --tags | sed \'s/\([^-]*-g\)/r\1/;s/-/./g\''));
+$gitVersion = trim(shell_exec('git -C '. __DIR__ .'/../.. describe --long --tags --dirty --broken'));
 
 return [
     'Git-Commit' => $gitVersion
