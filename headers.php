7172cf690bb9f3967222c48157f000a5150a4b17
Moritz 'e1mo' Fromm <git@e1mo.de>
2020-10-02 02:18:56 +0200

    Prefix header mit X

--- headers.php
+++ headers.php
@@ -3,5 +3,5 @@
 $gitVersion = trim(shell_exec('git -C '. __DIR__ .'/../.. describe --long --tags --dirty --broken'));
 
 return [
-    'Git-Commit' => $gitVersion
+    'X-Git-Commit' => $gitVersion
 ];
